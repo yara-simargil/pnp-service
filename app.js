74f4dfe908c9e43233cd24d3aea9b8569c914641
Olga Leus <yara@spotify.com>
2016-12-14 17:33:51 +0100

    Add allow headers, update metadata

--- app.js
+++ app.js
@@ -7,23 +7,28 @@ let app = module.exports = koa();
 
 let api = {
   list: function *() {
-    this.type = 'json';
     this.body = yield fs.readFile('data/characters/list.json', 'utf8');
   },
 
   get: function *(id) {
-    this.type = 'json';
     this.body = yield fs.readFile('data/characters/' + id + '.json', 'utf8');
   },
 
-  metadata: function *(system, type) {
-    this.type = 'json';
-    this.body = yield fs.readFile('data/' + system + '/charsheets/' + type + '.json', 'utf8');
+  metadata: function *(system) {
+    this.body = yield fs.readFile('data/metadata/' + system + '.json', 'utf8');
   }
 };
 
+app.use(function *(next) {
+  this.response.type = 'json';
+  this.response.set('Access-Control-Allow-Origin', '*');
+  this.response.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  this.response.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Authentication-Token, Access-Control-Request-Method');
+  yield next;
+});
+
 app.use(_.get('/characters', api.list));
 app.use(_.get('/characters/:id', api.get));
-app.use(_.get('/metadata/:system/:type', api.metadata));
+app.use(_.get('/metadata/:system', api.metadata));
 
 app.listen(8080);

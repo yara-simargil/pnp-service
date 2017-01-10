26dbc860739eadbc608ba7f68eeaf9bd7bea6c43
Olga Leus <yara@spotify.com>
2017-01-10 18:59:26 +0100

    Switched to koa-router, updated koa version.

--- app.js
+++ app.js
@@ -1,51 +1,58 @@
-let _ = require('koa-route');
-let koa = require('koa');
+let Koa = require('koa');
 let bodyParser = require('koa-bodyparser');
-let path = require('path');
-let fs = require('co-fs');
+let Router = require('koa-router');
+let fs = require('fs');
 
-let app = module.exports = koa();
+let app = new Koa();
+let router = new Router();
 
 let api = {
-  list: function *() {
-    this.response.type = 'json';
-    this.body = yield fs.readFile('data/characters/list.json', 'utf8');
+  list: function(ctx, next) {
+    ctx.response.type = 'json';
+    ctx.body = fs.readFileSync('data/characters/list.json', 'utf8');
   },
 
-  get: function *(id) {
-    this.response.type = 'json';
-    this.body = yield fs.readFile('data/characters/' + id + '.json', 'utf8');
+  get: function(ctx, next) {
+    let {id} = ctx.params;
+    ctx.response.type = 'json';
+    ctx.body = fs.readFileSync('data/characters/' + id + '.json', 'utf8');
   },
 
-  update: function *(id) {
-    let json = JSON.stringify(this.request.body);
-    yield fs.writeFile('data/characters/' + id + '.json', json, 'utf8', function (err, data) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(data);
+  update: function(ctx, next) {
+    let {id} = ctx.params;
+    let json = JSON.stringify(ctx.request.body);
 
-    });
-    this.body = 'Saved successfully';
+    if (!ctx.request.body || json.length < 3) {
+      ctx.body = 'No content received';
+      return;
+    }
+    fs.writeFileSync('data/characters/' + id + '.json', json, 'utf8');
+    ctx.body = 'Saved successfully';
   },
 
-  metadata: function *(system) {
-    this.response.type = 'json';
-    this.body = yield fs.readFile('data/metadata/' + system + '.json', 'utf8');
+  metadata: function(ctx, next) {
+    let {system} = ctx.params;
+    ctx.response.type = 'json';
+    ctx.body = fs.readFileSync('data/metadata/' + system + '.json', 'utf8');
   }
 };
 
-app.use(function *(next) {
-  this.response.set('Access-Control-Allow-Origin', '*');
-  this.response.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  this.response.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Authentication-Token, Access-Control-Request-Method');
-  yield next;
-});
-app.use(bodyParser());
+router.get('/characters', api.list)
+  .get('/characters/:id', api.get)
+  .post('/characters/:id', api.update)
+  .get('/metadata/:system', api.metadata);
 
-app.use(_.get('/characters', api.list));
-app.use(_.get('/characters/:id', api.get));
-app.use(_.post('/characters/:id', api.update));
-app.use(_.get('/metadata/:system', api.metadata));
+app.use((ctx, next) => {
+  return next().then(() => {
+    ctx.response.set('Access-Control-Allow-Origin', '*');
+    ctx.response.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    ctx.response.set('Access-Control-Allow-Headers',
+                     'Origin, X-Requested-With, Content-Type, Accept, X-Authentication-Token, Access-Control-Request-Method');
+  });
+});
+
+app.use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(8080);
